7af2e882008bf0d82f026cdde10c9b4f60427b61
gunhappy <phasin.s@ku.th>
2018-04-05 11:18:15 +0700

    switch screen

---
+++ Landing.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { Text, View, Image, Button } from 'react-native'
+import styles from './style/style'
+
+const image = require('./img/logo.png')
+
+class Landing extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image source={image} style={styles.imageStyle}/>
+                <Text> Hello I am here </Text>
+                <View style={styles.buttonContainer}>
+                    <Button 
+                        title='Click to go to next view'
+                        onPress={() => this.props.switchScreen('show')}
+                    />
+                </View>
+            </View>
+        )
+    }
+}
+
+export default Landing
---
+++ Show.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Text, View, Image, Button } from 'react-native'
+import styles from './style/style'
+
+const imgShow = require('./img/gtx2080.jpg')
+
+class Show extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image source={imgShow} style={styles.imageStyle}/>
+                <Button 
+                    title="Back to landing"
+                    onPress={() => this.props.switchScreen('landing')}
+                />
+            </View>
+        )
+    }
+}
+
+export default Show
